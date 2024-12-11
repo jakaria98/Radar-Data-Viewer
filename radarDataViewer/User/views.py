357d5a8f85c52bc3cd564812eb119279be60f109
jakaria98 <jhossain439@gmail.com>
2024-12-11 16:17:06 -0330

    new app for user created and all views added for user

---
+++ radarDataViewer/User/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+from django.shortcuts import get_object_or_404
+from rest_framework.authtoken.models import Token
+from .models import User
+from .serializers import UserSerializer, UpdateUserSerializer
+
+# Create your views here.
+@api_view(['POST'])
+def login(request):
+    user = get_object_or_404(User, username=request.data['username'])
+    if not user.check_password(request.data['password']):
+        return Response("missing user", status=status.HTTP_404_NOT_FOUND)
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(user)
+    return Response({'token': token.key, 'user': serializer.data})
+
+@api_view(['POST'])
+def register(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        user = User.objects.get(username=request.data['username'])
+        user.set_password(request.data['password'])
+        user.save()
+        token = Token.objects.create(user=user)
+        return Response({'token': token.key, 'user': serializer.data, "message" : "User created Succesfully"})
+    return Response(serializer.errors, status=status.HTTP_200_OK)
+
+########################## Not working
+########################## Not working
+########################## Not working
+@api_view(['PUT'])
+def update_user(request):
+    user = request.user
+    serializer = UpdateUserSerializer(user, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        # user = User.objects.get(username=request.data['username'])
+        # user.set_password(request.data['password'])
+        # user.save()
+        # token = Token.objects.create(user=user)
+        return Response({'user': serializer.data, "message" : "Updated User info Succesfully"})
+    return Response(serializer.errors, status=status.HTTP_200_OK)
+########################## Not working
+########################## Not working
+########################## Not working
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def test_token(request):
+    return Response("passed for {}".format(request.user.email))
