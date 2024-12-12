3f77cdc771431d352d3dfd40d388dae34eab348e
islam-monirul <islammonirul.cse@gmail.com>
2024-12-12 07:08:52 -0330

    design correction

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -12,9 +12,9 @@ const Header = () => {
 	};
 
 	return (
-		<header className="bg-headerFooter border-b-2 border-green-400 py-4 z-0">
+		<header className="bg-headerFooter border-b-2 border-green-400 py-4 sticky top-0 w-full z-10">
 			<div className="px-4 flex justify-between">
-				<h1 className="text-4xl font-bold text-green-400">
+				<h1 className="text-3xl font-bold text-green-400">
 					Radar Data Viewer
 				</h1>
 				<button className="glass-button" onClick={handleLogout}>
--- frontend/src/Pages/RegisterUser.jsx
+++ frontend/src/Pages/RegisterUser.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../Components/AuthContext";
+import Footer from "../Components/Footer";
 import Header from "../Components/Header";
 import RadarAnimation from "../Components/RadarAnimation";
 
@@ -35,102 +36,116 @@ export default function RegisterUser() {
 
 			<Header />
 
-			{/* Login Form */}
-			<div
-				className="w-[400px] bg-glass border-2 border-green-400 rounded-lg shadow-glass p-6 relative z-10"
-				style={{
-					backdropFilter: "blur(10px)",
-					background: "rgba(255, 255, 255, 0.1)",
-				}}
-			>
-				<h2 className="text-2xl font-bold text-green-400 text-center mb-6">
-					User Registration
-				</h2>
-				<form onSubmit={handleRegistration} className="space-y-4">
-					<div className="flex gap-4">
+			<main className="flex-grow flex flex-col justify-center items-center py-8 relative z-10">
+				<div
+					className="w-[400px] bg-glass border-2 border-green-400 rounded-lg shadow-glass p-2 relative z-10 mb-4"
+					style={{
+						backdropFilter: "blur(10px)",
+						background: "rgba(255, 255, 255, 0.1)",
+					}}
+				>
+					<h2 className="text-2xl font-bold text-green-400 text-center">
+						User Registration
+					</h2>
+				</div>
+				<div
+					className="w-[400px] bg-glass border-2 border-green-400 rounded-lg shadow-glass p-6 relative z-10"
+					style={{
+						backdropFilter: "blur(10px)",
+						background: "rgba(255, 255, 255, 0.1)",
+					}}
+				>
+					<form
+						onSubmit={handleRegistration}
+						className="space-y-4"
+					>
+						<div className="flex gap-4">
+							<div>
+								<label
+									htmlFor="firstName"
+									className="block text-green-400 mb-2"
+								>
+									First Name
+								</label>
+								<input
+									type="text"
+									id="firstName"
+									className="w-full px-4 py-2 bg-black text-white border border-green-400 rounded-md focus:outline-none focus:ring-2 focus:ring-green-400"
+									placeholder="First Name"
+								/>
+							</div>
+							<div>
+								<label
+									htmlFor="lastName"
+									className="block text-green-400 mb-2"
+								>
+									Last Name
+								</label>
+								<input
+									type="text"
+									id="lastName"
+									className="w-full px-4 py-2 bg-black text-white border border-green-400 rounded-md focus:outline-none focus:ring-2 focus:ring-green-400"
+									placeholder="Last Name"
+								/>
+							</div>
+						</div>
 						<div>
 							<label
-								htmlFor="firstName"
+								htmlFor="email"
 								className="block text-green-400 mb-2"
 							>
-								First Name
+								Email Address
 							</label>
 							<input
-								type="text"
-								id="firstName"
+								type="email"
+								id="email"
 								className="w-full px-4 py-2 bg-black text-white border border-green-400 rounded-md focus:outline-none focus:ring-2 focus:ring-green-400"
-								placeholder="First Name"
+								placeholder="Enter your email address"
 							/>
 						</div>
 						<div>
 							<label
-								htmlFor="lastName"
+								htmlFor="username"
 								className="block text-green-400 mb-2"
 							>
-								Last Name
+								Username
 							</label>
 							<input
 								type="text"
-								id="lastName"
+								id="username"
 								className="w-full px-4 py-2 bg-black text-white border border-green-400 rounded-md focus:outline-none focus:ring-2 focus:ring-green-400"
-								placeholder="Last Name"
+								placeholder="Enter your username"
 							/>
 						</div>
-					</div>
-					<div>
-						<label
-							htmlFor="email"
-							className="block text-green-400 mb-2"
-						>
-							Email Address
-						</label>
-						<input
-							type="email"
-							id="email"
-							className="w-full px-4 py-2 bg-black text-white border border-green-400 rounded-md focus:outline-none focus:ring-2 focus:ring-green-400"
-							placeholder="Enter your email address"
-						/>
-					</div>
-					<div>
-						<label
-							htmlFor="username"
-							className="block text-green-400 mb-2"
-						>
-							Username
-						</label>
-						<input
-							type="text"
-							id="username"
-							className="w-full px-4 py-2 bg-black text-white border border-green-400 rounded-md focus:outline-none focus:ring-2 focus:ring-green-400"
-							placeholder="Enter your username"
-						/>
-					</div>
 
-					<div>
-						<label
-							htmlFor="password"
-							className="block text-green-400 mb-2"
-						>
-							Password
-						</label>
-						<input
-							type="password"
-							id="password"
-							className="w-full px-4 py-2 bg-black text-white border border-green-400 rounded-md focus:outline-none focus:ring-2 focus:ring-green-400"
-							placeholder="Enter your password"
-						/>
-					</div>
+						<div>
+							<label
+								htmlFor="password"
+								className="block text-green-400 mb-2"
+							>
+								Password
+							</label>
+							<input
+								type="password"
+								id="password"
+								className="w-full px-4 py-2 bg-black text-white border border-green-400 rounded-md focus:outline-none focus:ring-2 focus:ring-green-400"
+								placeholder="Enter your password"
+							/>
+						</div>
 
-					<div>
-						<button
-							type="submit"
-							className="w-full px-4 py-2 bg-green-400 text-black font-semibold rounded-md shadow hover:bg-green-500 transition-all duration-300"
-						>
-							Register
-						</button>
-					</div>
-				</form>
-			</div>
+						<div>
+							<button
+								type="submit"
+								className="w-full px-4 py-2 bg-green-400 text-black font-semibold rounded-md shadow hover:bg-green-500 transition-all duration-300"
+							>
+								Register
+							</button>
+						</div>
+					</form>
+				</div>
+			</main>
+
+			<Footer />
 		</div>
 	);
 }
