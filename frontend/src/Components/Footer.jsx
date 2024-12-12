f51b3d173f2c363ca9360654fde675fd7df678de
islam-monirul <islammonirul.cse@gmail.com>
2024-12-12 07:57:13 -0330

    footer text color correction

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
 	return (
 		<footer className="bg-headerFooter border-t-2 border-green-400 py-4 mt-auto w-full z-10">
-			<p className="text-center text-gray-400">
+			<p className="text-center text-green-400">
 				© 2024 Radar Data Viewer
 			</p>
 		</footer>
