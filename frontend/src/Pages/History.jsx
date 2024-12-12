424d68e22da5e276e17102960d1d47881b70eb45
islam-monirul <islammonirul.cse@gmail.com>
2024-12-12 15:09:55 -0330

    authentication for history page added

--- frontend/src/Pages/History.jsx
+++ frontend/src/Pages/History.jsx
@@ -1,3 +1,16 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../Components/AuthContext";
+
 export default function History() {
+	const navigate = useNavigate();
+	const { authState } = useAuth();
+	useEffect(() => {
+		console.log(authState.isAdmin);
+		if (!authState.isLoggedIn) {
+			console.log("Not Logged In");
+			navigate("/login");
+			return;
+		}
+	}, [authState.isLoggedIn, navigate]);
 	return <></>;
 }
