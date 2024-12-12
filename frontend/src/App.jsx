1c9094a115ef9ebda51a20114d2579dd9bf1cd36
islam-monirul <islammonirul.cse@gmail.com>
2024-12-11 21:43:49 -0330

    routing added for register-user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import DataViewer from "./Pages/DataViewer";
 import LoginPage from "./Pages/LoginPage";
+import RegisterUser from "./Pages/RegisterUser";
 import UploadFile from "./Pages/UploadFile";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
 				<Route path="/" element={<DataViewer />} />
 				<Route path="/upload" element={<UploadFile />} />
 				<Route path="/login" element={<LoginPage />} />
+				<Route path="/addUser" element={<RegisterUser />} />
 			</Routes>
 		</Router>
 	);
